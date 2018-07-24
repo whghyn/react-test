533c3fbd22cd2de29bfb376f302d028048be2ac1
huangyanan04 <huangyanan04@gmail.com>
2018-07-24 19:54:24 +0800

    第2次修改

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      // 第一次修改
+      // 第2次修改
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
           <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React2</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
