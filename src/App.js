426eb50f9b195f7d27d234563ae2e60019635aac
huangyanan04 <huangyanan04@gmail.com>
2018-07-25 10:12:05 +0800

    Revert "第一次修改"
    This reverts commit 088f598a7a516c7da1742ff479d1131fae59214d.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      // 第一次修改
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
