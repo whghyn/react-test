da67c515ed46828e215bfb451443db432c72f482
huangyanan04 <huangyanan04@gmail.com>
2018-10-14 11:00:24 +0800

    初始化

--- src/pags/App.js
+++ src/pags/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
+import Calculator from './Calculator/Calculator';
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-=======
+
       // 第2次修改
->>>>>>> 533c3fbd22cd2de29bfb376f302d028048be2ac1
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-<<<<<<< HEAD
-=======
-          <h1 className="App-title">Welcome to React</h1>
-          <h1 className="App-title">Welcome to React2</h1>
->>>>>>> 533c3fbd22cd2de29bfb376f302d028048be2ac1
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </div> 
+      
+
     );
   }
 }
